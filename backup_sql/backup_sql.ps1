707349992402ab33144a25c415720624cc398330
Dzmitry Bichko <bichko@AZ.LOCAL>
2023-08-23 09:48:33 +0300

    add looping every databese meat* expect meat_ubuntu

--- backup_sql/backup_sql.ps1
+++ backup_sql/backup_sql.ps1
@@ -4,6 +4,7 @@ $User = 'sa'
 $backupDirectory='C:\var\opt\mssql\data\backup'
 # Convert plain text into a secure string
 $Pass = Get-Content D:\BackUp1с83\sql.txt |ConvertTo-SecureString
+$Pass = ConvertTo-SecureString 'Password_01' -AsPlainText –Force
 #supply the $Pass variable as SecureString for the password
 $Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User,$Pass
 #Build the connection
@@ -16,25 +17,27 @@ $server.ConnectionContext.set_Login($Credentials.UserName)
 $server.ConnectionContext.set_SecurePassword($credentials.Password)
 # Connect to the Server and get a few properties
 #$dbs = $server.Databases | where { $_.IsSystemObject -eq $False }
-$dbs = $server.Databases | where { $_.Name -like "meat*" }
+$dbs = $server.Databases | where { ($_.Name -like "meat*") -and ($_.Name -notlike "meat_ubuntu") }
 #Looping through every databases
-#$dbName = 'meat'
-$timestamp = Get-Date -format yyyy-MM-dd-HHmmss
-$targetPath = $backupDirectory + "\" + $dbName + "_" + $timestamp + ".bak"
-#Define tackup object that will be used for the database backup    
-$smoBackup = New-Object ("Microsoft.SqlServer.Management.Smo.Backup")
-# To perform the full backup  z
-$smoBackup.Action = "Database"
-# To turnoff  Differential backup     
-$smoBackup.Incremental = $False
-#Specify the backup description so that we know what this backup is and when it was taken.
-$smoBackup.BackupSetDescription = "Full Backup of " + $dbName
-$smoBackup.BackupSetName = $dbName + " Backup"
-$smoBackup.Database = $dbName
-$smoBackup.MediaDescription = "Disk"
-# Add a backup device using AddDevice() method to a file.   
-$smoBackup.Devices.AddDevice($targetPath, "File")
-#Initiate Database Backup
-$smoBackup.SqlBackup($server)
-write-host "FULL back up $dbName ($serverName) to $targetPath"
-#Copy-Item -Path $targetPath -Destination 'D:\MSSQL\BACKUP\meat\'
+foreach ($database in $dbs)
+{
+    $dbName = $database.Name
+    $timestamp = Get-Date -format yyyy-MM-dd-HHmmss
+    $targetPath = $backupDirectory + "\" + $dbName + "_" + $timestamp + ".bak"
+    #Define tackup object that will be used for the database backup    
+    $smoBackup = New-Object ("Microsoft.SqlServer.Management.Smo.Backup")
+    # To perform the full backup  z
+    $smoBackup.Action = "Database"
+    # To turnoff  Differential backup     
+    $smoBackup.Incremental = $False
+    #Specify the backup description so that we know what this backup is and when it was taken.
+    $smoBackup.BackupSetDescription = "Full Backup of " + $dbName
+    $smoBackup.BackupSetName = $dbName + " Backup"
+    $smoBackup.Database = $dbName
+    $smoBackup.MediaDescription = "Disk"
+    # Add a backup device using AddDevice() method to a file.   
+    $smoBackup.Devices.AddDevice($targetPath, "File")
+    #Initiate Database Backup
+    $smoBackup.SqlBackup($server)
+    write-host "FULL back up $dbName ($serverName) to $targetPath"
+}
